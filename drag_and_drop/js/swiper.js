41de4973c0c9f92b46443fe1bff143ede4b4594a
kanako katsumata <may.2s.gp31@gmail.com>
2019-07-04 17:42:00 +0900

    rename

--- drag_and_drop/js/swiper.js
+++
@@ -1,9 +0,0 @@
-var mySwiper = new Swiper('.swiper-container', {
-	loop: false,
-	slidesPerView: 1,
-	spaceBetween: 10,
-	centeredSlides: true,
-	nextButton: '.swiper-button-next',
-	prevButton: '.swiper-button-prev',
-	pagination: '.swiper-pagination',
-});
