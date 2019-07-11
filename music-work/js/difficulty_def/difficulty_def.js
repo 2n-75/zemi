1eb3ddaff738dd08920b29ef5ff5be8d03eee23c
kanako katsumata <may.2s.gp31@gmail.com>
2019-07-11 20:18:40 +0900

    Amax Smaxを求める式を追加

--- music-work/js/difficulty_def/difficulty_def.js
+++ music-work/js/difficulty_def/difficulty_def.js
@@ -1,35 +1,37 @@
 /*
 譜面の難易度を判定する
 */
-// 音符の長さと難易度のデータ
-const NOTES_DATA = [
-	{ Dc: 1, L: 4 },
-	{ Dc: 2, L: 2 },
-	{ Dc: 4, L: 1 },
-	{ Dc: 8, L: 0.5 }
-];
-// 楽譜のデータ
-const TEST_DATA = [1, 0.5, 0.5, 1, 2]; // それぞれの音符のL
 
-// もっともよく使われている音符の平均難易度を求める
-function calc_Dp(_data) {
+import { average } from "./calculate";
+import { NOTES_DATA, TEST_DATA } from './const';
+
+/* 実行 */
+function main(notesArray) {
+	// もっともよく使われている音符の平均難易度と平均音価
+	const { Dp, aveL } = calc_average(notesArray);
+	console.log("一番多く使われている音符の平均難易度は" + Dp + " 平均音価は" + aveL);
+
+	const { Amax, Smax } = calc_maxLimit(aveL, Dp, notesArray);
+	console.log("Amax:" + Amax + " Smax:" + Smax);
+}
+main(TEST_DATA);
+
+// もっともよく使われている音符の平均音価と平均難易度を求める
+function calc_average(_data) {
 	const countsList = appearanceCounter(_data);
 	// countsList.map(function (c) { return c.count; }) でcountsListからcountの列だけ取り出した配列ができる
-	const maxCount = Math.max.apply(null, countsList.map(function (c) { return c.count; }));
-	let denominator = 0; // 分母
-	for (let i = 0; i < countsList.length; i++) {
-		if (countsList[i].count == maxCount) {
-			denominator += countsList[i].Dc;
-		}
-	}
-	return denominator / _data.length;
+	const maxCount = Math.max.apply(null, countsList.map(function (x) { return x.count; }));
+	// 最頻値のリスト
+	const L_mode = countsList.filter(x => x.count === maxCount).map(function (x) { return x.L; });
+	const Dc_mode = countsList.filter(x => x.count === maxCount).map(function (x) { return x.Dc; });
+	return { Dp: average(Dc_mode), aveL: average(L_mode) }
 }
 
 // 音符の難易度と出現回数のjsonを返す
 function appearanceCounter(_data) {
-	counts = [];
+	let counts = [];
 	for (let i = 0; i < NOTES_DATA.length; i++) {
-		counts.push({ Dc: NOTES_DATA[i].Dc, count: 0 });
+		counts.push({ Dc: NOTES_DATA[i].Dc, L: NOTES_DATA[i].L, count: 0 });
 	}
 	for (let j = 0; j < counts.length; j++) {
 		for (let i = 0; i < _data.length; i++) {
@@ -53,6 +55,11 @@ function L_to_Dc(l) {
 	return dc;
 }
 
-// もっともよく使われている音符の平均難易度
-const Dp = calc_Dp(TEST_DATA);
-console.log("一番多く使われている音符の平均難易度は" + Dp);
+// 1回あたりの比較で増加または減少され得る値の上限値Amax, Smaxを求める
+function calc_maxLimit(aveL, Dp, notesArray) {
+	let notesNum = 0;
+	for (let i = 0; i < notesArray.length; i++) {
+		if (notesArray[i] != aveL) notesNum += 1;
+	}
+	return { Amax: (10 - Dp) / notesNum, Smax: (Dp - 1) / notesNum };
+}
